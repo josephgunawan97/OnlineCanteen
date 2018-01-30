95ae8316cb64d454e63f21a3bae8807f39ab2ae2
rickhenhermawan <rickhen123@yahoo.com>
2018-01-31 02:16:25 +0700

    Update UI + Coding Image from Gallery

--- onlineCanteen/app/src/main/java/com/example/asus/onlinecanteen/RegisterActivity.java
+++ onlineCanteen/app/src/main/java/com/example/asus/onlinecanteen/RegisterActivity.java
@@ -1,15 +1,49 @@
 package com.example.asus.onlinecanteen;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    ImageView imageView;
+    Button button;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        imageView = (ImageView)findViewById(R.id.imageinput);
+        button =  (Button)findViewById(R.id.browse);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             openGallery();
+            }
+        });
+    }
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
     }
 }
