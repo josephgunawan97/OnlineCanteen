89a84259f2ada1ea883045757f414d5e0905db87
Steven Albert <steven.a.540@hotmail.com>
2018-02-07 22:23:30 +0700

    Update register upload profile picture

--- onlineCanteen/app/src/main/java/com/example/asus/onlinecanteen/RegisterActivity.java
+++ onlineCanteen/app/src/main/java/com/example/asus/onlinecanteen/RegisterActivity.java
@@ -1,10 +1,15 @@
 package com.example.asus.onlinecanteen;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.Image;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -33,6 +38,7 @@ import com.google.firebase.storage.UploadTask;
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = RegisterActivity.class.getSimpleName();
+    private static final int REQUEST_READ_EXTERNAL_STORAGE = 999;
 
     ImageView imageView;
     Button button, submitbtn;
@@ -109,11 +115,15 @@ public class RegisterActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
-                            addAdditionalUserInformation();
-                            // GO TO MAIN PAGE - after success
-                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                            startActivity(intent);
-                            finish();
+                            if(imageUri != null) {
+                                if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                                    requestReadStoragePermission();
+                                } else {
+                                    addAdditionalUserInformation();
+                                }
+                            } else {
+                                backToLoginScreen();
+                            }
                         } else {
                             emailET.setText("");
                             emailET.setError("Email is registered");
@@ -130,21 +140,23 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
                             user = mAuth.getCurrentUser();
-                            if (user!=null && imageUri!=null) {
-                                // MASIH ERROR
-/*                                uploadImage();
-                                UserProfileChangeRequest profile = new UserProfileChangeRequest.Builder()
-                                        .setDisplayName(usernameET.getText().toString())
-                                        .setPhotoUri(Uri.parse(profPicUrl))
-                                        .build();
-                                user.updateProfile(profile);
-*/
-                                String uid = user.getUid();
-                                Log.d(TAG, uid);
-                                User userInfo = new User(nimET.getText().toString(), "USER", phoneET.getText().toString());
-                                userReferences = FirebaseDatabase.getInstance().getReference("users").child(uid);
-                                userReferences.setValue(userInfo);
-                            }
+
+                            UserProfileChangeRequest.Builder profileBuilder = new UserProfileChangeRequest.Builder();
+                            profileBuilder.setDisplayName(usernameET.getText().toString());
+
+                            UserProfileChangeRequest profileChangeRequest = profileBuilder.build();
+                            user.updateProfile(profileChangeRequest);
+
+                            String uid = user.getUid();
+                            User userInfo = new User(nimET.getText().toString(), "USER", phoneET.getText().toString());
+                            userReferences = FirebaseDatabase.getInstance().getReference("users").child(uid);
+                            userReferences.setValue(userInfo);
+
+                            if (user != null && imageUri != null &&
+                                    ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                                uploadImage();
+                            } else backToLoginScreen();
+
                         } else {
                             Toast.makeText(RegisterActivity.this, "Failed to update profile.", Toast.LENGTH_SHORT).show();
                         }
@@ -154,18 +166,29 @@ public class RegisterActivity extends AppCompatActivity {
 
     //To upload image
     private void uploadImage() {
+        Log.d(TAG, "Uploading...");
         StorageReference profileImageRef = FirebaseStorage.getInstance().getReference("profilepics/"+System.currentTimeMillis()+".jpg");
         if (imageUri!=null){
-            // SecurityException
             profileImageRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                     profPicUrl = taskSnapshot.getDownloadUrl().toString();
+                    Log.d(TAG, "Success in uploading");
+                    UserProfileChangeRequest.Builder profileBuilder = new UserProfileChangeRequest.Builder();
+                    if(profPicUrl != null) {
+                        Log.d(TAG, "Photo is taken");
+                        profileBuilder.setPhotoUri(Uri.parse(profPicUrl));
+                    }
+                    UserProfileChangeRequest profileChangeRequest = profileBuilder.build();
+                    user.updateProfile(profileChangeRequest);
+
+                    backToLoginScreen();
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
                     Toast.makeText(getApplicationContext(),"Image failed to upload",Toast.LENGTH_LONG).show();
+                    backToLoginScreen();
                 }
             });
         }
@@ -223,5 +246,29 @@ public class RegisterActivity extends AppCompatActivity {
        // databaseUsers.push().setValue(user);
     //}
 
+    private void requestReadStoragePermission() {
+        ActivityCompat.requestPermissions(RegisterActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                REQUEST_READ_EXTERNAL_STORAGE);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_READ_EXTERNAL_STORAGE) {
+            if(grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                addAdditionalUserInformation();
+            }
+        }
+    }
+
+    private void backToLoginScreen() {
+        if(mAuth != null) {
+            mAuth.signOut();
+        }
+        // GO TO LOGIN PAGE - after success
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
