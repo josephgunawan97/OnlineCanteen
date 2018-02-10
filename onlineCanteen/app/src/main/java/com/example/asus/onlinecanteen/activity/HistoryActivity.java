c8a6bb5eab1770ae2de7cf5048ab82c92e6ef90c
rickhenhermawan <rickhen123@yahoo.com>
2018-02-10 17:46:45 +0700

    Update History UI, Login Upgrade

---
+++ onlineCanteen/app/src/main/java/com/example/asus/onlinecanteen/activity/HistoryActivity.java
@@ -0,0 +1,18 @@
+package com.example.asus.onlinecanteen.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.asus.onlinecanteen.R;
+
+/**
+ * Created by Rickhen Hermawan on 10/02/2018.
+ */
+
+public class HistoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
