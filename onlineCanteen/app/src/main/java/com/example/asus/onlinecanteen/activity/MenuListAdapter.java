61749a67c9bde441dd76fefffc1bf44a86017b92
Clarissa <cveronicakusuma@yahoo.com>
2018-02-14 22:58:45 +0700

    Add Increase and Decrease Order Function + Updated MenuListAdapter

--- onlineCanteen/app/src/main/java/com/example/asus/onlinecanteen/activity/MenuListAdapter.java
+++ onlineCanteen/app/src/main/java/com/example/asus/onlinecanteen/activity/MenuListAdapter.java
@@ -16,65 +16,76 @@ import com.example.asus.onlinecanteen.model.Product;
 import java.util.List;
 
 public class MenuListAdapter extends ArrayAdapter<Product> {
-    //Increase & Decrease Item's Quantity
-    TextView quantityOrder;
-    Button decreaseOrder, increaseOrder;
     int qty;
 
     public MenuListAdapter(Activity context, List<Product> products) {
         super(context, R.layout.menu_adapter_list, products);
     }
 
-
     public View getView(int position,View view,ViewGroup parent) {
-
+        final OrderHolder holder;
         if(view == null) {
             LayoutInflater inflater = ((Activity) getContext()).getLayoutInflater();
             view = inflater.inflate(R.layout.menu_adapter_list, parent,false);
+
+            holder = new OrderHolder();
+
+            // Initialize views
+            holder.txtTitle = view.findViewById(R.id.itemName);
+            holder.imageView = view.findViewById(R.id.icon);
+            holder.extratxt = view.findViewById(R.id.price);
+            holder.seller = view.findViewById(R.id.seller);
+            holder.quantityOrder = view.findViewById(R.id.quantityOrder);
+            holder.increaseOrder = view.findViewById(R.id.increaseOrder);
+            holder.decreaseOrder = view.findViewById(R.id.decreaseOrder);
+
+            view.setTag(holder);
+        }else{
+            holder = (OrderHolder)view.getTag();
         }
         // Get product
         Product product = getItem(position);
 
-        // Initialize views
-        TextView txtTitle = view.findViewById(R.id.itemName);
-        ImageView imageView = view.findViewById(R.id.icon);
-        TextView extratxt = view.findViewById(R.id.price);
-        TextView seller = view.findViewById(R.id.seller);
-
-
-        txtTitle.setText(product.getName());
-        extratxt.setText("Rp " + product.getPrice());
-        seller.setText("Seller : " + product.getTokoId());
+        //Set Texts
+        holder.txtTitle.setText(product.getName());
+        holder.extratxt.setText("Rp " + product.getPrice());
+        holder.seller.setText("Seller : " + product.getTokoId());
+        holder.quantityOrder.setText(String.valueOf(qty));
 
         // Get image
         if(product.getImageUrl() != null) {
-            Glide.with(imageView.getContext())
+            Glide.with(holder.imageView.getContext())
                     .load(product.getImageUrl())
-                    .into(imageView);
+                    .into(holder.imageView);
         }
 
-        //Initialize Increase Button, Decrease Button, Qty TextView, and Click Listener
-        quantityOrder = view.findViewById(R.id.quantityOrder);
-        quantityOrder.setText(String.valueOf(qty));
-        increaseOrder = view.findViewById(R.id.increaseOrder);
-        increaseOrder.setOnClickListener(new View.OnClickListener(){
+        //Increase Order
+        holder.increaseOrder.setOnClickListener(new View.OnClickListener(){
             public void onClick (View v){
                 qty++;
-                quantityOrder.setText(String.valueOf(qty));
-
+                holder.quantityOrder.setText(String.valueOf(qty));
             }
         });
 
-        decreaseOrder = view.findViewById(R.id.decreaseOrder);
-        decreaseOrder.setOnClickListener(new View.OnClickListener(){
+        //Decrease Order
+        holder.decreaseOrder.setOnClickListener(new View.OnClickListener(){
             public void onClick (View v){
                 if(qty > 0) {
                     qty--;
-                    quantityOrder.setText(String.valueOf(qty));
+                    holder.quantityOrder.setText(String.valueOf(qty));
                 }
             }
         });
-
         return view;
+    }
+
+    static class OrderHolder {
+        TextView quantityOrder;
+        Button decreaseOrder;
+        Button increaseOrder;
+        TextView txtTitle;
+        ImageView imageView;
+        TextView extratxt;
+        TextView seller;
     }
 }
