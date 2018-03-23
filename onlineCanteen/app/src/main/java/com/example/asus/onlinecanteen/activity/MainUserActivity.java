8293fedb45b2f6ad83970ef293daaa2a41fe1698
josephgunawan97 <joseph.gunawan97@gmail.com>
2018-03-23 10:06:52 +0700

    Add User and Transaction QR

--- onlineCanteen/app/src/main/java/com/example/asus/onlinecanteen/activity/MainUserActivity.java
+++ onlineCanteen/app/src/main/java/com/example/asus/onlinecanteen/activity/MainUserActivity.java
@@ -2,6 +2,7 @@ package com.example.asus.onlinecanteen.activity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -15,10 +16,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.asus.onlinecanteen.R;
@@ -33,6 +36,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainUserActivity extends AppCompatActivity implements MainUserFragment.StoreClickHandler {
 
@@ -85,8 +93,8 @@ public class MainUserActivity extends AppCompatActivity implements MainUserFragm
         NavigationView navigationView = findViewById(R.id.main_user_navigation_view);
         View header = navigationView.getHeaderView(0);
         userWallet = header.findViewById(R.id.user_wallet);
-        TextView username =  header.findViewById(R.id.user_navigation_user_name) ;
-        TextView email =  header.findViewById(R.id.user_navigation_user_email) ;
+        TextView username = header.findViewById(R.id.user_navigation_user_name);
+        TextView email = header.findViewById(R.id.user_navigation_user_email);
         ImageView profilePicture = header.findViewById(R.id.user_navigation_user_picture);
 
         //Get wallet amount
@@ -94,8 +102,7 @@ public class MainUserActivity extends AppCompatActivity implements MainUserFragm
         walletRef.child(user.getUid()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists())
-                {
+                if (dataSnapshot.exists()) {
                     userWallet.setText(dataSnapshot.getValue().toString());
                 }
             }
@@ -107,11 +114,10 @@ public class MainUserActivity extends AppCompatActivity implements MainUserFragm
         });
 
 
-
         // Set username and email
         username.setText(user.getDisplayName());
         email.setText(user.getEmail());
-        if(user.getPhotoUrl() != null) {
+        if (user.getPhotoUrl() != null) {
             Glide.with(this)
                     .load(user.getPhotoUrl())
                     .into(profilePicture);
@@ -120,10 +126,9 @@ public class MainUserActivity extends AppCompatActivity implements MainUserFragm
 
     @Override
     public void onBackPressed() {
-        if(fragmentManager.getBackStackEntryCount() == 1) {
+        if (fragmentManager.getBackStackEntryCount() == 1) {
             finish();
-        }
-        else super.onBackPressed();
+        } else super.onBackPressed();
     }
 
     @Override
@@ -141,7 +146,7 @@ public class MainUserActivity extends AppCompatActivity implements MainUserFragm
     }
 
     //User Logout
-    public void logout(){
+    public void logout() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(R.string.signOut_confirmation)
                 .setCancelable(false)
@@ -182,10 +187,48 @@ public class MainUserActivity extends AppCompatActivity implements MainUserFragm
                 case R.id.navigation_menu_logout:
                     logout();
                     break;
+                case R.id.navigation_menu_qr:
+                    showQR();
+                    break;
                 default:
                     break;
             }
             return true;
         }
     }
-}
+
+    private void showQR() {
+        Bitmap bitmap;
+        // TODO Auto-generated method stub
+        AlertDialog.Builder alertadd = new AlertDialog.Builder(
+                this);
+        alertadd.setTitle("QR Code");
+
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View view = factory.inflate(R.layout.qr_layout, null);
+
+        String text2Qr = user.getUid().toString();
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text2Qr, BarcodeFormat.QR_CODE,300,300);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            ImageView image= (ImageView) view.findViewById(R.id.imageView);
+            image.setImageBitmap(bitmap);
+        }  catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+        alertadd.setView(view);
+        alertadd.setNeutralButton("Close", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dlg, int sumthin) {
+
+            }
+        });
+
+        alertadd.show();
+
+    }
+
+    }
+
--- onlineCanteen/app/src/main/java/com/example/asus/onlinecanteen/fragment/TransactionDetailFragment.java
+++ onlineCanteen/app/src/main/java/com/example/asus/onlinecanteen/fragment/TransactionDetailFragment.java
@@ -1,20 +1,35 @@
 package com.example.asus.onlinecanteen.fragment;
 
 
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.asus.onlinecanteen.R;
 import com.example.asus.onlinecanteen.adapter.TransactionDetailAdapter;
 import com.example.asus.onlinecanteen.model.Transaction;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,6 +45,7 @@ public class TransactionDetailFragment extends Fragment {
     private RecyclerView itemsRecyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private TextView totalAmountTextView;
+    private ImageButton QR;
 
     public TransactionDetailFragment() {}
 
@@ -62,5 +78,69 @@ public class TransactionDetailFragment extends Fragment {
 
         itemsRecyclerView.setLayoutManager(layoutManager);
         itemsRecyclerView.setAdapter(detailAdapter);
+
+        QR = (ImageButton) view.findViewById(R.id.qr_button);
+        QR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            showQR();
+            }
+        });
     }
+
+    private void showQR() {
+
+        FirebaseDatabase.getInstance().getReference().child("transactions").orderByChild("uid").equalTo(transaction.getUid()).addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.i(TransactionDetailFragment.class.getSimpleName() , " ADD12 "+ dataSnapshot.getValue() );
+                for(DataSnapshot productSnapshot : dataSnapshot.getChildren())
+                {
+                    if(productSnapshot.child("sid").getValue().equals(transaction.getSid()))
+                    {
+
+                        Bitmap bitmap;
+                        // TODO Auto-generated method stub
+                        AlertDialog.Builder alertadd = new AlertDialog.Builder(
+                                getContext());
+                        alertadd.setTitle("QR Code");
+
+                        LayoutInflater factory = LayoutInflater.from(getContext());
+                        final View view = factory.inflate(R.layout.qr_layout, null);
+                        String text2Qr = productSnapshot.getKey();
+                        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                        try {
+                            BitMatrix bitMatrix = multiFormatWriter.encode(text2Qr, BarcodeFormat.QR_CODE,300,300);
+                            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                            bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                            ImageView image= (ImageView) view.findViewById(R.id.imageView);
+                            image.setImageBitmap(bitmap);
+                        }  catch (WriterException e) {
+                            e.printStackTrace();
+                        }
+
+                        alertadd.setView(view);
+                        alertadd.setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dlg, int sumthin) {
+
+                            }
+
+                        });
+                        alertadd.show();
+                    }
+                    }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
+
 }
